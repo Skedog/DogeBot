6b38517d4c901ea2223067d6e27878235a5f091f
Skedog <skedog@skedog.com>
2017-06-25 17:42:09 -0400

    Massive refactor of all front end JS and related express processing

--- express.js
+++ express.js
@@ -96,51 +96,73 @@ var connect = function(db,dbConstants) {
 			}
 		}
 
-		var checkUserModStatus = function(req) {
+		var checkLoginStatus = function(req) {
 			return new Promise((resolve, reject) => {
+				var token = req.cookies.token;
 				if (req.cookies.userDetails) {
 					var userDetails = req.cookies.userDetails.split(',');
-					if (req.channel != undefined) {
-						var channelToCheckMods = req.channel; //from URL, never has #
-					} else if (req.body.channel != undefined) {
-						var channelToCheckMods = req.body.channel.substring(1); //has #, needs to be removed
-					} else {
-						var channelToCheckMods = userDetails[2].substring(1); //has #, needs to be removed
-					}
+					var currentChannel = userDetails[2];
 					var twitchUserID = userDetails[3];
-					if (!!twitchUserID) {
-						runSQL('select','channels',{twitchUserID:parseInt(twitchUserID)},'',db).then(results => { //select logged in channel to get username
-							if (results) {
-								loggedInChannel = results[0]['ChannelName'].substring(1);
-								twitchClient.mods(channelToCheckMods).then(modRes => {
-									var a = modRes.indexOf(loggedInChannel);
-									if (a > -1) {
-										if (req.originalUrl.includes('/')) {
-											resolve(req.originalUrl.split('/'));
-										} else {
-											resolve(req.originalUrl);
-										}
-									} else {
-										if (channelToCheckMods == loggedInChannel) {
+					checkUserLogin(db,token,twitchUserID).then(isLoggedIn => {
+						resolve(true);
+					}).catch(function(err) {
+						reject(false);
+					});
+				} else {
+					reject(false);
+				}
+			});
+		}
+
+		var checkModStatus = function(req) {
+			return new Promise((resolve, reject) => {
+				checkLoginStatus(req).then(results => {
+					if (req.cookies.userDetails) {
+						var userDetails = req.cookies.userDetails.split(',');
+						if (req.channel != undefined) {
+							var channelToCheckMods = req.channel; //from URL, never has #
+						} else if (req.body.channel != undefined) {
+							var channelToCheckMods = req.body.channel.substring(1); //has #, needs to be removed
+						} else {
+							var channelToCheckMods = userDetails[2].substring(1); //has #, needs to be removed
+						}
+						var twitchUserID = userDetails[3];
+						if (!!twitchUserID) {
+							runSQL('select','channels',{twitchUserID:parseInt(twitchUserID)},'',db).then(results => { //select logged in channel to get username
+								if (results) {
+									loggedInChannel = results[0]['ChannelName'].substring(1);
+									twitchClient.mods(channelToCheckMods).then(modRes => {
+										var a = modRes.indexOf(loggedInChannel);
+										if (a > -1) {
 											if (req.originalUrl.includes('/')) {
 												resolve(req.originalUrl.split('/'));
 											} else {
 												resolve(req.originalUrl);
 											}
 										} else {
-											//username isn't in the mod list, they are not a mod
-											resolve('');
-										}
-									};
-								}).catch(function(err) {
-									reject(err);
-								});
-							}
-						})
-					}
-				} else {
-					resolve('');
-				}
+											if (channelToCheckMods == loggedInChannel) {
+												if (req.originalUrl.includes('/')) {
+													resolve(req.originalUrl.split('/'));
+												} else {
+													resolve(req.originalUrl);
+												}
+											} else {
+												//username isn't in the mod list, they are not a mod
+												resolve('');
+											}
+										};
+									}).catch(function(err) {
+										reject(err);
+									});
+								}
+							})
+						}
+					} else {
+						resolve('');
+					};
+				}).catch(function(err) {
+					reject(err);
+				});
 			});
 		}
 
@@ -202,21 +224,21 @@ var connect = function(db,dbConstants) {
 		})
 
 		app.get('/moderation/:channel*?', function(req, res){
-			checkUserModStatus(req).then(results => {
+			checkModStatus(req).then(results => {
 				if (results) {
 					res.render(results[1] + '.html',{passedUser: req.channel});
 				} else {
 					res.redirect('/dashboard');
 				}
 			}).catch(function(err) {
-				log.error(err);
+				res.redirect('/logout');
 			});
 		}, function (req, res) {
 			res.redirect('/logout');
 		})
 
 		app.post('/removesong', function(req, res) {
-			checkUserModStatus(req).then(results => {
+			checkModStatus(req).then(results => {
 				if (results) {
 					dataToUse = {};
 					var query2 = {channel:req.body.channel,songID:req.body.songToRemove};
@@ -341,37 +363,63 @@ var connect = function(db,dbConstants) {
 		});
 
 		app.post('/updatesettings', function(req, res) {
-			var dataToUse = {};
-			dataToUse["duplicateSongDelay"] = parseInt(req.body.duplicateSongDelay,10);
-			dataToUse["songNumberLimit"] = parseInt(req.body.songNumberLimit);
-			dataToUse["maxSongLength"] = parseInt(req.body.maxSongLength);
-			runSQL('update','channels',{ChannelName:req.body.channel},dataToUse,db).then(results => {
-				res.send('updated');
+			checkModStatus(req).then(results => {
+				if (results) {
+					var dataToUse = {};
+					dataToUse["duplicateSongDelay"] = parseInt(req.body.duplicateSongDelay,10);
+					dataToUse["songNumberLimit"] = parseInt(req.body.songNumberLimit);
+					dataToUse["maxSongLength"] = parseInt(req.body.maxSongLength);
+					runSQL('update','channels',{ChannelName:req.body.channel},dataToUse,db).then(results => {
+						res.send('updated');
+					});
+				} else {
+					res.send('error');
+				}
+			}).catch(function(err) {
+				res.send('error');
 			});
 		});
 
 		app.post('/updatevolume', function(req, res) {
-			var query = {ChannelName:req.body.channel};
-			var dataToUse = {};
-			dataToUse["volume"] = req.body.volume;
-			runSQL('update','channels',query,dataToUse,db).then(results => {
-				res.send('');
+			checkModStatus(req).then(results => {
+				if (results) {
+					var query = {ChannelName:req.body.channel};
+					var dataToUse = {};
+					dataToUse["volume"] = req.body.volume;
+					runSQL('update','channels',query,dataToUse,db).then(results => {
+						res.send('');
+					});
+				} else {
+					res.send('error');
+				}
+			}).catch(function(err) {
+				res.send('error');
 			});
 		});
 
 		app.post('/updatemusicstatus', function(req, res) {
-			var query = {ChannelName:req.body.channel};
-			var dataToUse = {};
-			dataToUse["musicStatus"] = req.body.musicStatus;
-			runSQL('update','channels',query,dataToUse,db).then(results => {
-				res.send('');
+			checkModStatus(req).then(results => {
+				if (results) {
+					var query = {ChannelName:req.body.channel};
+					var dataToUse = {};
+					dataToUse["musicStatus"] = req.body.musicStatus;
+					runSQL('update','channels',query,dataToUse,db).then(results => {
+						res.send('');
+					});
+				} else {
+					res.send('error');
+				}
+			}).catch(function(err) {
+				res.send('error');
 			});
 		});
 
 		app.post('/joinchannel', function(req, res) {
 			twitch.joinSingleChannel(req.body.channel).then(results => {
 				res.send('joined');
-			})
+			}).catch(function(err) {
+				res.send(err);
+			});
 		});
 
 		app.post('/partchannel', function(req, res) {
@@ -398,36 +446,42 @@ var connect = function(db,dbConstants) {
 		});
 
 		app.post('/loadnextsong', function(req, res) {
-			var query = {channel:req.body.channel};
-			runSQL('select','songs',query,'',db).then(results => {
+			checkModStatus(req).then(results => {
 				if (results) {
-					var songToRemove = results[0]['_id'];
-					var dataToUse = {};
-					var query = {};
-					var songToReturn = '';
-					if (results[1] !== undefined) {
-						var songToUpdateSortOrder = results[1]['_id'];
-						songToReturn = results[1]['songID'];
-						dataToUse = {};
-						query = {channel:req.body.channel,_id:songToUpdateSortOrder};
-						dataToUse["sortOrder"] = 100000;
-						runSQL('update','songs',query,dataToUse,db).then(results => {}); //update command
-					}
-					dataToUse = {};
-					var query2 = {channel:req.body.channel,_id:songToRemove};
-					runSQL('delete','songs',query2,dataToUse,db).then(results => {}); //update command
+					var query = {channel:req.body.channel};
+					runSQL('select','songs',query,'',db).then(results => {
+						if (results) {
+							var songToRemove = results[0]['_id'];
+							var dataToUse = {};
+							var query = {};
+							var songToReturn = '';
+							if (results[1] !== undefined) {
+								var songToUpdateSortOrder = results[1]['_id'];
+								songToReturn = results[1]['songID'];
+								dataToUse = {};
+								query = {channel:req.body.channel,_id:songToUpdateSortOrder};
+								dataToUse["sortOrder"] = 100000;
+								runSQL('update','songs',query,dataToUse,db).then(results => {}); //update command
+							}
+							dataToUse = {};
+							var query2 = {channel:req.body.channel,_id:songToRemove};
+							runSQL('delete','songs',query2,dataToUse,db).then(results => {}); //update command
 
-					dataToUse = {};
-					query = {ChannelName:req.body.channel};
-					dataToUse["tempSortVal"] = 199999;
-					runSQL('update','channels',query,dataToUse,db).then(results => {}); //update command
+							dataToUse = {};
+							query = {ChannelName:req.body.channel};
+							dataToUse["tempSortVal"] = 199999;
+							runSQL('update','channels',query,dataToUse,db).then(results => {}); //update command
 
-					if (songToReturn) {
-						res.send(songToReturn);
-					} else {
-						res.send('empty');
-					}
-				}
+							if (songToReturn) {
+								res.send(songToReturn);
+							} else {
+								res.send('empty');
+							}
+						}
+					});
+				};
+			}).catch(function(err) {
+				res.send('error');
 			});
 		});
 
--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,35 +1,39 @@
+function isNumeric(n) {
+	return !isNaN(parseFloat(n)) && isFinite(n);
+};
+
 function getUrlVars() {
-    var vars = [], hash;
-    if (window.location.href.indexOf('#')) {
-    	var hashes = window.location.href.slice(window.location.href.indexOf('#') + 1).split('&');
-    } else {
-    	var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-    };
-    for(var i = 0; i < hashes.length; i++)
-    {
-        hash = hashes[i].split('=');
-        vars.push(hash[0]);
-        vars[hash[0]] = hash[1];
-    }
-    return vars;
+	var vars = [], hash;
+	if (window.location.href.indexOf('#')) {
+		var hashes = window.location.href.slice(window.location.href.indexOf('#') + 1).split('&');
+	} else {
+		var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+	};
+	for(var i = 0; i < hashes.length; i++)
+	{
+		hash = hashes[i].split('=');
+		vars.push(hash[0]);
+		vars[hash[0]] = hash[1];
+	}
+	return vars;
 };
 
 function readCookie(cname) {
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length,c.length);
-        }
-    }
-    return "";
+	var name = cname + "=";
+	var ca = document.cookie.split(';');
+	for(var i = 0; i <ca.length; i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') {
+			c = c.substring(1);
+		}
+		if (c.indexOf(name) == 0) {
+			return c.substring(name.length,c.length);
+		}
+	}
+	return "";
 }
 
-userDetails = decodeURIComponent(readCookie("userDetails")).split(',');
+var userDetails = decodeURIComponent(readCookie("userDetails")).split(',');
 if (typeof userDetails[2] != 'undefined') {
 	$.ajax({
 		url: '/loggedinnav',
@@ -48,52 +52,247 @@ if (typeof userDetails[2] != 'undefined') {
 	});
 };
 
-setTimeout(function() {
-    if ($( window ).width() < 767) {
-        $('.datatable').DataTable().destroy();
-    }
-}, 100);
-$( window ).resize(function() {
-    if ($( window ).width() > 767) {
-        // if (!$.fn.DataTable.isDataTable('.datatable')) {
-            $('.datatable').DataTable().destroy();
-            $('.datatable').DataTable({
-                "lengthMenu": [[25, 50, -1], [25, 50, "All"]]
-            });
-        // } else {}
-    } else {
-        $('.datatable').DataTable().destroy();
-    }
-});
-
 function loadSonglist(data,page) {
-    $.ajax({
-        url: '/getsonglist',
-        data: data,
-        type: 'POST',
-        success: function(data) {
-            if (data != '') {
-                var contentData = '';
-                $('.datatable').DataTable().destroy();
-                $.each(data, function(key, value) {
-                    if (key == 0) {
-                        $('.currentsong').html('<strong>Song Title:</strong> ' + data[0]['songTitle'] + '<br><strong>Requested By:</strong> ' + data[0]['whoRequested']);
-                    }
-                    if (page == 'moderation') {
-                        contentData = contentData + '<tr><td>' + (key + 1) + '</td><td>' + data[key]['songTitle'] + '</td><td><a href="https://youtu.be/' + data[key]['songID'] + '" target="_blank">' + data[key]['songID'] + '</a></td><td>' + data[key]['whoRequested'] + '</td><td><input type="button" value="Remove" id="' + data[key]['songID'] + '" class="removeButton" /></td></tr>';
-                    } else {
-                        contentData = contentData + '<tr><td>' + (key + 1) + '</td><td>' + data[key]['songTitle'] + '</td><td><a href="https://youtu.be/' + data[key]['songID'] + '" target="_blank">' + data[key]['songID'] + '</a></td><td>' + data[key]['whoRequested'] + '</td></tr>';;
-                    };
-                });
-                $('.datatable tbody').html(contentData);
-                $('.datatable').DataTable({
-                    "lengthMenu": [[5, 25, 50, -1], [5, 25, 50, "All"]]
-                });
-                $('.datatable').show();
-            } else {
-                $('.datatable tbody').hide();
-                $('.moderation-songlist').html("Currently no songs in the queue!");
-            };
-        }
-    });
+	return new Promise((resolve, reject) => {
+		$.ajax({
+			url: '/getsonglist',
+			data: data,
+			type: 'POST',
+			success: function(data) {
+				if (data != '') {
+					var contentData = '';
+					$.each(data, function(key, value) {
+						if (key == 0) {
+							$('.currentsong').html('<strong>Song Title:</strong> ' + data[0]['songTitle'] + '<br><strong>Requested By:</strong> ' + data[0]['whoRequested']);
+						}
+						if (page == 'moderation') {
+							contentData = contentData + '<tr><td>' + (key + 1) + '</td><td>' + data[key]['songTitle'] + '</td><td><a href="https://youtu.be/' + data[key]['songID'] + '" target="_blank">' + data[key]['songID'] + '</a></td><td>' + data[key]['whoRequested'] + '</td><td><input type="button" value="Remove" id="' + data[key]['songID'] + '" class="removeButton" /></td></tr>';
+						} else {
+							contentData = contentData + '<tr><td>' + (key + 1) + '</td><td>' + data[key]['songTitle'] + '</td><td><a href="https://youtu.be/' + data[key]['songID'] + '" target="_blank">' + data[key]['songID'] + '</a></td><td>' + data[key]['whoRequested'] + '</td></tr>';;
+						};
+					});
+					resolve(contentData);
+				} else {
+					resolve(false);
+				};
+			}
+		});
+	});
 }
+
+function loadSongCache(data) {
+	return new Promise((resolve, reject) => {
+		$.ajax({
+			url: '/getsongcache',
+			data: data,
+			type: 'POST',
+			success: function(data) {
+				if (data != '') {
+					var contentData = '';
+					$('.datatable').DataTable().destroy();
+					$.each(data, function(key, value) {
+						contentData = contentData + '<tr><td>' + (key + 1) + '</td><td>' + data[key]['songTitle'] + '</td><td><a href="https://youtu.be/' + data[key]['songID'] + '" target="_blank">' + data[key]['songID'] + '</a></td></tr>';
+					});
+					resolve(contentData);
+				} else {
+					resolve(false);
+				};
+			}
+		});
+	});
+}
+
+function getCommands(channelName) {
+	return new Promise((resolve, reject) => {
+		buildChannelDataString(channelName).then(channelData => {
+			$.ajax({
+				url: '/getcommands',
+				data: channelData,
+				type: 'POST',
+				success: function(data) {
+					if (data != '') {
+						var contentData = '';
+						$.each(data, function(key, value) {
+							contentData = contentData + '<tr><td>' + data[key]['trigger'] + '</td><td>' + data[key]['chatmessage'] + '</td><td>' + data[key]['commandcounter'] + '</td><td>' + data[key]['permissionsLevel'] + '</td></tr>';
+						});
+						resolve(contentData);
+					} else {
+						resolve(false);
+					};
+				}
+			});
+		});
+	});
+}
+
+function buildDataTable(passedData,elementToUse,startSize) {
+	$('.datatable').DataTable().destroy();
+	$(elementToUse + ' tbody').html(passedData);
+	if (startSize == '25') {
+		$(elementToUse).DataTable({
+			"lengthMenu": [[25, 50, -1], [25, 50, "All"]]
+		});
+	} else if (startSize == '5') {
+		$(elementToUse).DataTable({
+			"lengthMenu": [[5, 10, 25, -1], [5, 10, 25, "All"]]
+		});
+	}
+	$(elementToUse).show();
+}
+
+function joinChannel(channelName) {
+	return new Promise((resolve, reject) => {
+		buildChannelDataString(channelName).then(channelData => {
+			$.ajax({
+				url: '/joinchannel',
+				data: channelData,
+				type: 'POST',
+				success: function(data) {
+					resolve(data);
+				}
+			});
+		});
+	});
+}
+
+function leaveChannel(channelName) {
+	return new Promise((resolve, reject) => {
+		buildChannelDataString(channelName).then(channelData => {
+			$.ajax({
+				url: '/partchannel',
+				data: channelData,
+				type: 'POST',
+				success: function(data) {
+					resolve(data);
+				}
+			});
+		});
+	});
+}
+
+function checkIfInChannel(channelName) {
+	return new Promise((resolve, reject) => {
+		buildChannelDataString(channelName).then(channelData => {
+			$.ajax({
+				url: '/checkchannelstatus',
+				data: channelData,
+				type: 'POST',
+				success: function(data) {
+					resolve(data);
+				}
+			});
+		});
+	});
+};
+
+function getChannelName(urlUser) {
+	return new Promise((resolve, reject) => {
+		if (urlUser != 'undefined') {
+			resolve(urlUser);
+		} else {
+			resolve(userDetails[2]);
+		};
+	});
+};
+
+function buildChannelDataString(channelName) {
+	return new Promise((resolve, reject) => {
+		if (channelName.includes('#')) {
+			resolve('channel=' + channelName);
+		} else {
+			resolve('channel=#' + channelName);
+		};
+	});
+}
+
+function removeSong(songToRemove,channelName,loggedInChannel) {
+	return new Promise((resolve, reject) => {
+		$.ajax({
+			url: '/removesong',
+			data: 'songToRemove=' + songToRemove + '&channel=' + channelName + '&loggedInChannel=' + loggedInChannel,
+			type: 'POST',
+			success: function(data) {
+				resolve(data);
+			}
+		});
+	});
+};
+
+function checkForSonglistChanges(channelData,songlist,dataTableStartSize,page) {
+	this['checkSkippedSongsInterval'] = setInterval(function() {
+		loadSonglist(channelData,page).then(songlistReload => {
+			if (songlist != songlistReload) {
+				buildDataTable(songlistReload,'.datatable',dataTableStartSize);
+				songlist = songlistReload;
+			}
+		});
+	}, 1000);
+}
+
+function getVolume(channelData) {
+	return new Promise((resolve, reject) => {
+		$.ajax({
+			url: '/getvolume',
+			data: channelData,
+			type: 'POST',
+			success: function(data) {
+				resolve(data[0]['volume']);
+			}
+		})
+	});
+}
+
+function updateVolume(channelData,newVol) {
+	return new Promise((resolve, reject) => {
+		$.ajax({
+			url: '/updatevolume',
+			data: channelData + '&volume=' + newVol,
+			type: 'POST',
+			success: function(data) {
+				resolve(newVol);
+			}
+		});
+	});
+}
+
+function getMusicStatus(channelData) {
+	return new Promise((resolve, reject) => {
+		$.ajax({
+			url: '/getmusicstatus',
+			data: channelData,
+			type: 'POST',
+			success: function(data) {
+				resolve(data);
+			}
+		})
+	});
+}
+
+
+function updateMusicStatus(channelData,musicStatus) {
+	$.ajax({
+		url: '/updatemusicstatus',
+		data: channelData + '&musicStatus=' + musicStatus,
+		type: 'POST'
+	});
+}
+
+$(document).ready(function() {
+	setTimeout(function() {
+		if ($( window ).width() < 767) {
+			$('.datatable').DataTable().destroy();
+		}
+	}, 100);
+	$( window ).resize(function() {
+		if ($( window ).width() > 767) {
+	        // if (!$.fn.DataTable.isDataTable('.datatable')) {
+	        	$('.datatable').DataTable().destroy();
+	        	$('.datatable').DataTable({
+	        		"lengthMenu": [[25, 50, -1], [25, 50, "All"]]
+	        	});
+	        // } else {}
+	    } else {
+	    	$('.datatable').DataTable().destroy();
+	    }
+	});
+});
